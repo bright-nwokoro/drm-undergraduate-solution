2acda469c953802292c583bb1481fd1ce6782934
Bright Nwokoro <brightnwokoro13@gmail.com>
2021-07-04 12:45:51 +0100

    Updated ASGI

--- drm_systen/drm_systen/asgi.py
+++ drm_systen/drm_systen/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drm_systen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drm_systen.drm_systen.settings')
 
 application = get_asgi_application()
