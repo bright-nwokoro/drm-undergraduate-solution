9366afb59384a481e01d173ddd2ac04598d32ba6
Bright Nwokoro <brightnwokoro13@gmail.com>
2021-07-04 13:08:48 +0100

    Updated WSGI

--- drm_systen/drm_systen/wsgi.py
+++ drm_systen/drm_systen/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drm_systen.drm_systen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drm_systen.settings')
 
 application = get_wsgi_application()
